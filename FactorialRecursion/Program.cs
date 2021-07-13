39e0e741c0d19f2c637905b4f93a32cc98537852
rodrigo costa <rodrigo.alves@bairesdev.com>
2021-07-12 23:53:05 -0300

    Add project files.

---
+++ FactorialRecursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int factorial(int n)
+    {
+        return (n == 1) ? n : (n * factorial(n - 1));
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        Console.WriteLine(result);
+
+//        textWriter.Flush();
+//        textWriter.Close();
+    }
+}
